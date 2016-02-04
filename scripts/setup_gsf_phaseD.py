c744ab50ebe625d851dd0814ab5ee8e60ee1e9d6
Andrew Walker <a.walker@leeds.ac.uk>
2016-02-04 10:22:33 +0000

    For now, stick my phaseD GSF setup script in here.

---
+++ scripts/setup_gsf_phaseD.py
@@ -0,0 +1,25 @@
+#!/usr/bin/env python
+
+import numpy as np
+
+from pyDis.atomic import crystal as cry
+from pyDis.atomic import castep_utils as cas
+from pyDis.atomic import atomistic_utils as atm
+from pyDis.pn import  gsf_setup as gsf
+
+path_to_phased = './'
+sc_length = 6 # Number of unit cells along z
+              # must be an even number in this case.
+spacing = (8, 8) # Number of points in a and b
+
+phaseD = cas.CastepCrystal()
+sys_info = cas.parse_castep('phaseD.cell', phaseD,
+             path=path_to_phased)
+phaseD.translate_cell(0.02*cry.ei(3))
+
+slab = gsf.make_slab(phaseD,sc_length, free_atoms=['Mg', 'O', 'H'])
+atm.scale_kpoints(sys_info['mp_kgrid'],np.array([1,1,sc_length]))
+
+gsf.gamma_surface(slab,spacing,cas.write_castep,sys_info,
+                   basename='gsf_phaseD',suffix='cell', limits=(0.5, 0.5),
+                   mkdir=True)
