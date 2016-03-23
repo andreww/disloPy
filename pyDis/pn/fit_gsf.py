c75ec8f742ed670cf147833cce2a40f728b4260e
u4868117@anu.edu.au <u4868117@anu.edu.au>
2016-03-23 12:48:09 +1100

    Merged <plotgammasurface> with <fit_gsf>.

--- pyDis/pn/fit_gsf.py
+++ pyDis/pn/fit_gsf.py
@@ -13,6 +13,7 @@ import sys
 sys.path.append('/home/richard/code_bases/dislocator2/')
 
 from mpl_toolkits.mplot3d import Axes3D
+from matplotlib import cm
 from scipy.interpolate import RectBivariateSpline, interp1d
 
 from pyDis.atomic import atomistic_utils as atm
@@ -252,3 +253,37 @@ def contour_plot(X, Y, Z, xlabel, ylabel, size):
     ax.set_yticks([])
     fig.colorbar(cont)
     return 
+    
+def plot_gamma_surface(gsf_file, a, b, plotname='gamma_surface.tif', 
+                                                     angle=np.pi/2.):
+    
+    # read in gridded data and units                                                
+    gsf_grid, units = read_numerical_gsf(gsf_file)
+    print(gsf_grid)
+    
+    gsf_func = spline_fit2d(gsf_grid, a, b, angle=angle, two_planes=True)
+    
+    #!!! Should make the number of increments adaptive; still this probably
+    #!!! provides sufficient sampling density for display purposes
+    x = np.linspace(0., a, 100)
+    y = np.linspace(0., b, 100)
+    
+    X, Y = np.meshgrid(x, y)
+    Z = np.zeros(np.shape(X))
+    
+    for i in range(len(Z)):
+        for j in range(len(Z[0])):
+            Z[i, j] = gsf_func(X[i, j], Y[i, j])
+            
+    fig = plt.figure()
+    ax = fig.gca(projection='3d')
+    
+    #cset = ax.contourf(X, Y, Z, 50, zdir='z', cmap=cm.coolwarm, offset=-0.1)
+    
+    ax.plot_surface(X, Y, Z, cstride=2, rstride=2, cmap=cm.coolwarm)
+    ax.set_zlim(Z.min(), Z.max()+0.05*(Z.max()-Z.min()))
+    ax.set_xlim(0., a)
+    ax.set_ylim(0., b)
+    plt.show()
+    
+    return Z
