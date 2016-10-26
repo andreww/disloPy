Small fixes to <peierls_barrier>, which have had an outsized effect on the accuracy of the Peierls stress calculation.

pyDis/pn/peierls_barrier.py
@@ -112,8 +112,8 @@ def stressed_dislocation(params, n_funcs, max_x, energy_function, K, b, spacing,
                                                             disl_type, dims)
     # obtain the relaxed structure of the dislocation in the applied stress 
     # field
-    new_par = fmin_slsqp(total_opt_stress, params, eqcons=constraints, 
-                                    args=in_args, bounds=lims, iprint=0)
+    new_par = fmin_slsqp(total_opt_stress, params, eqcons=constraints, args=in_args,
+                                                     bounds=lims, iprint=0, acc=1e-14)
      
     # calculate total self-interaction + misfit + stress energy                                   
     E = total_opt_stress(new_par, *in_args)       
@@ -207,7 +207,7 @@ def taup(dis_parameters, max_x, gsf_func, K, b, spacing,  dims=1, disl_type=None
     
     rho = pn1.rho(u, r)
     #d_max = (rho*r[1:]/rho.sum()).sum()
-    cm0 = com(rho, r, b)
+    cm0 = pn1.center_of_mass(rho, r, b)
     # apply stress to the dislocation, starting with the positive direction
     new_par = dis_parameters
     for s in stresses:
@@ -224,14 +224,14 @@ def taup(dis_parameters, max_x, gsf_func, K, b, spacing,  dims=1, disl_type=None
                 us = uxs
             else: # screw
                 us = uys
-                    
-        rhos = pn1.rho(us, r)
-        d_current = (rhos*r[1:]/rhos.sum()).sum()
         
-        #if abs(d_current-d_max) >= threshold:
-        #    tau_p_plus = s
-        #    break
-        if abs(com(rhos, r, b) - cm0) >= threshold*spacing:
+        # calculate the stressed dislocation's centre            
+        rhos = pn1.rho(us, r)        
+        cm_new = pn1.center_of_mass(rhos, r, b) 
+        # calculate distance of dislocation density c.o.m from the location of
+        # the unstressed dislocation, recording the value if it exceeds specified
+        # threshold
+        if abs(cm_new - cm0) >= threshold:
             tau_p_plus = s
             break
 
@@ -251,18 +251,18 @@ def taup(dis_parameters, max_x, gsf_func, K, b, spacing,  dims=1, disl_type=None
             else: # screw
                 us = uys
         
+        # calculate centre of mass of the stressed dislocation
         rhos = pn1.rho(us, r)
-        d_current = (rhos*r[1:]/rhos.sum()).sum()
+        cm_new = pn1.center_of_mass(rhos, r, b)
         
-        #if abs(d_current-d_max) >= threshold:
-        #    tau_p_minus = -s
-        #    break
-        if abs(com(rhos, r, b) - cm0) >= threshold*spacing:
+        # calculate distance of dislocation density c.o.m from the location of
+        # the unstressed dislocation, recording the value if it exceeds specified
+        # threshold
+        if abs(cm_new - cm0) >= threshold:
             tau_p_minus = -s
             break
                    
     peierls_stresses = [tau_p_minus, tau_p_plus]
-    peierls_stresses.sort()
     
     if in_GPa:
         # express Peierls stresses in GPa
@@ -292,3 +292,43 @@ def peierls_barrier(taup, b, in_GPa=True):
 def sig_figs(value, n_figs):
     x = '{{:.{}f}}'.format(n_figs).format(value)
     return float(x)
+    
+### PEIERLS BARRIER BY TRANSLATION AND OPTIMIZATION ###
+
+def com_constraint(par, *args):
+    '''Constrains the location of the centre of mass of the 
+    dislocation density distribution.
+    '''
+    
+    # extract sim. parameters
+    n_funcs = args[0]
+    N = args[1]
+    energy_function = args[2]
+    b = args[3]
+    spacing = args[4]
+    K = args[5]
+    fix_centre = args[6]
+    dimensions = args[7]
+    if dimensions == 2:
+        disl_type = args[8]
+    
+    # calculate dislocation density
+    if dimensions == 1:
+        u = pn1.get_u1d(par, b, spacing, N)
+    elif dimensions == 2:
+        ux, uy = pn2.get_u2d(par, b, spacing, N, disl_type)
+        if disl_type.lower() == 'edge':
+            u = ux
+        elif disl_type.lower() == 'screw':
+            u = uy
+     
+    # calculate the dislocation density distribution
+    r = np.arange(-N*spacing, N*spacing, spacing)
+    rho = pn1.rho(u, r)
+    
+    # calculate dislocation density c.o.m.
+    com = pn1.center_of_mass(rho, r, b)
+    # return distance between current and desired centre of 
+    # mass
+    return (com - fix_centre)
+

pyDis/pn/pn_1D.py
@@ -266,23 +266,24 @@ def elastic_energy(A, x0, c, b=1, K=1):
             E += Ai*Aj*np.log((xi-xj)**2+(ci+cj)**2)
     return -0.5*K*E*b**2
     
-def misfit_energy(A, x0, c, N, energy_function, b, spacing, shift=0.):
+def misfit_energy(A, x0, c, N, energy_function, b, spacing, translate=0.):
     '''Calculates the inelastic energy associated with a dislocation defined
     by the parameters A, x0, and c and lying in the plane whose misfit energy
-    is given by <energy_function>.
+    is given by <energy_function>. <translate> translates the summation points
+    relative to the crystal lattice.
     '''
     
-    r = spacing*(np.arange(-N, N))
+    r = spacing*(np.arange(-N, N))+translate
     u = u_field(r, A, x0, c, b)
     Em = energy_function(u).sum()*spacing
     return Em
     
-def total_energy(A, x0, c, N, energy_function, b, spacing, K=1):
+def total_energy(A, x0, c, N, energy_function, b, spacing, K=1, translate=0.):
     '''Return the total elastic and inelastic (ie. misfit) energy of the 
     dislocation.
     '''
     
-    Em = misfit_energy(A, x0, c, N, energy_function, b, spacing)
+    Em = misfit_energy(A, x0, c, N, energy_function, b, spacing, translate=translate)
     E_elast = elastic_energy(A, x0, c, b, K)
     return Em + E_elast
     
@@ -299,7 +300,7 @@ def total_optimizable(params, *args):
     spacing = args[4]
     K = args[5]
         
-    # extract parameters and symmetrise, if required
+    # extract dislocation parameters
     A = params[:n_funcs]
     x0 = params[n_funcs:2*n_funcs]
     c = params[2*n_funcs:]
@@ -331,8 +332,8 @@ def cons_func(inparams, *args):
     A = inparams[:n_funcs]
     return 1.-sum(A)
 
-def dislocation1d(N, max_x, energy_function, lims, noopt, use_sym, b, spacing, K,
-                                                                      inpar=None):
+def dislocation1d(N, max_x, energy_function, lims, use_sym, b, spacing, K,
+                                                  inpar=None, noopt=False):
     '''Optimise structure of dislocation with Burgers vector <b> and misfit energy
     given by <energy_function>.
     '''
@@ -396,8 +397,8 @@ def run_monte1d(n_iter, N, K, max_x=100, energy_function=test_gamma, noopt=False
         if noisy and i % 100 == 0:
             print("Starting iteration {}...".format(i))
 
-        E, x_try = dislocation1d(N, max_x, energy_function, lims, noopt, use_sym,
-                                                    b, spacing, K, inpar=params)
+        E, x_try = dislocation1d(N, max_x, energy_function, lims, use_sym, b,
+                                       spacing, K, inpar=params, noopt=noopt)
 
         # check that the output parameters are physically reasonable
         is_valid = check_parameters1d(x_try, N, lims)

pyDis/pn/pn_2D.py
@@ -90,7 +90,8 @@ def elastic_energy2d(A, x0, c, b=1, K=[1, 1]):
     E = pn1.elastic_energy(A1, x01, c1, b, Ke) + pn1.elastic_energy(A2, x02, c2, b, Ks)
     return E
     
-def misfit_energy2d(A, x0, c, N, energy_function, b, spacing, shift=[0., 0.5]):
+def misfit_energy2d(A, x0, c, N, energy_function, b, spacing, shift=[0., 0.5], 
+                                                                translate=0.):
     '''Defaults shift corresponds to screw dislocation.
     
     0 <= xc <= 1
@@ -98,19 +99,21 @@ def misfit_energy2d(A, x0, c, N, energy_function, b, spacing, shift=[0., 0.5]):
     
     A1, x01, c1, A2, x02, c2 = unzip_parameters(A, x0, c)
     
-    r = spacing*(np.arange(-N, N))
+    r = spacing*(np.arange(-N, N))+translate
     ux = pn1.u_field(r, A1, x01, c1, b, bc=shift[0])
     uz = pn1.u_field(r, A2, x02, c2, b, bc=shift[1])
     Em = energy_function(ux, uz).sum()*spacing
 
     return Em
     
-def total_energy2d(A, x0, c, N, energy_function, K, b, spacing, shift=[0., 0.5]):
+def total_energy2d(A, x0, c, N, energy_function, K, b, spacing, shift=[0., 0.5], 
+                                                                   translate=0.):
     '''Calculates the total energy of the 2D dislocationd density distribution
     defined by <A>, <x0>, and <c>. Defaults to screw dislocation: K = [Ke, Ks]
     '''
     
-    Em = misfit_energy2d(A, x0, c, N, energy_function, b, spacing, shift=shift)
+    Em = misfit_energy2d(A, x0, c, N, energy_function, b, spacing, shift=shift,
+                                                           translate=translate)
     E_el = elastic_energy2d(A, x0, c, b, K)
     return Em + E_el
     