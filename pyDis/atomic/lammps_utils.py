956bcb35269e7ee83615e84ca55abde9df1cd6d8
u4868117@anu.edu.au <u4868117@anu.edu.au>
2017-04-24 14:23:28 +1000

    More work on LAMMPS fix.

--- pyDis/atomic/lammps_utils.py
+++ pyDis/atomic/lammps_utils.py
@@ -15,6 +15,7 @@ from numpy.linalg import norm
 
 from pyDis.atomic import crystal as cry
 from pyDis.atomic import atomistic_utils as atm
+from pyDis.atomic import transmutation as mutate
 
 class LammpsAtom(cry.Atom):
     '''Same as <cry.Atom>, but with charge.
@@ -66,10 +67,11 @@ class LammpsAtom(cry.Atom):
 
         if not (q is None):
             outstream.write(atom_format.format(self._index, self.getSpecies(), 
-                                      self.q, coords[0], coords[1], coords[2]))
+                                   self.q, coords[0]*scalex, coords[1]*scaley, 
+                                                              coords[2]*scalez))
         else:
             outstream.write(atom_format.format(self._index, self.getSpecies(),
-                                        coords[0], coords[1], coords[2]))
+                          coords[0]*scalex, coords[1]*scaley, coords[2]*scalez))
 
         if add_constraints:
             # add constraints if non-trivial 
@@ -184,24 +186,78 @@ def write_lammps(outstream, struc, sys_info, defected=True, do_relax=True, to_ca
     because atomic coordinates in LAMMPS are, by default, given in cartesian
     coordinates, the write function here sets the default value of <to_cart>
     to be <True>.
+    
+    #!!!At present, only crystals with orthogonal lattice vectors are supported.
     '''
 
 
-    outstream.write('This is the first line of a LAMMPS file')
+    outstream.write('This is the first line of a LAMMPS file\n\n')
+    
+    # insert any impurities
+    if not (impurities is None):
+        if mutate.is_single(impurities):
+            mutate.cell_defect(struc, impurities, use_displaced=defected)
+        elif mutate.is_coupled(impurities);
+            mutate.cell_defect_cluster(struc, impurities, use_displaced=defected)
+        else:
+            raise TypeError("Supplied defect not of type <Impurity>/<CoupledImpurity>")   
 
     # calculate total number of atoms in <lmp_struc> plus any impurities
-    n_atoms = len()
+    outstream.write('  {} atoms\n\n'.format(len(struc)))
+    
+    # number of distinct elements
+    outstream.write('  {} atom types\n\n'.format(struc.number_of_elements()))
 
-    # index to keep track of how many atoms have been written to file (makes it
-    # easier to keep track of impurities)
-    current_index = 1
-    lattice = lmp_struc.getLattice()
-
+    # simulation cell dimensions 
+    lattice = struc.getLattice()
+    outstream.write(' 0. {:.6f} xlo xhi\n'.format(lattice[0, 0]))
+    outstream.write(' 0. {:.6f} ylo yhi\n'.format(lattice[1, 1]))
+    outstream.write(' 0. {:.6f} zlo zhi\n'.format(lattice[2, 2]))
+    outstream.write(' {:.3f} {:.3f} {:.3f} xy xz yz\n\n'.format(lattice[1, 0],
+                                                lattice[2, 0], lattice[2, 1])
+    
+    # write atoms to file
     outstream.write('Atoms\n')
-    for i, atom in enumerate(lmp_struc):
-        lmp_struc.write('{} '.format(i))
+    for i, atom in enumerate(struc):
+        atom.set_index(i)
         atom.write(outstream, lattice=lattice, defected=defected, to_cart=to_cart)
+        
+    # atomic masses
+    outstream.write('\nMasses\n\n')
+    for species in sys_info['masses']:
+        outstream.write('{} {:.2f}\n'.format(species[0], species[1]))
 
     pass
     
-def run_lammps(
+def run_lammps(lammps_exec, basename, nproc=1, para_exec='mpiexec', set_omp=False,
+                                                                  omp_threads=1):
+    '''Runs a lammps simulation. Since lammps is a parallel code, there is the 
+    option to run it in parallel, with the number of processors given by <nproc>.    
+    '''
+    
+    # check the number of processors used -> if > 1, use parallel executable
+    # (default MPI)
+    if nproc < 1:
+        raise ValueError("Number of processors must be positive.")
+    elif nproc > 1:
+        use_para = True
+    else:
+        use_para = False
+        
+    # set number of openMPI threads per process (if > 1)
+    if use_para and set_omp and omp_threads >= 1:        
+        os.putenv('OMP_NUM_THREADS', str(int(omp_threads)))
+        
+    # input and output files
+    lin = open('in.{}'.format(basename), 'r')
+    lout = open('out.{}'.format(basename), 'w')
+    
+    # run lammps    
+    if use_para:
+        subprocess.call([para_exec, '-np', str(nproc), lammps_exec], stdin=lin,
+                                                                    stdout=lout)
+    else:
+        subprocess.call(lammps_exec, stdin=lin, stdout=lout)
+        
+    lin.close()
+    lout.close()
