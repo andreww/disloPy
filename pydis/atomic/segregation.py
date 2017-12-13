ceefcb40e26335517a229547b6859f27970edde4
u4868117@anu.edu.au <u4868117@anu.edu.au>
2017-12-13 14:58:00 +1100

    Made the range on the plotted segregation energies symmetric about zero.

--- pydis/atomic/segregation.py
+++ pydis/atomic/segregation.py
@@ -198,7 +198,7 @@ def write_energies(outname, site_info, e_excess, e_seg, pars=None):
     
 ### PLOTTING FUNCTIONS
 
-def plot_energies_contour(sites, e_seg, figname, r, cmtype='viridis', refine=False,
+def plot_energies_contour(sites, e_seg, figname, r, cmtype='bwr', refine=False,
                                  units='eV', figformat='tif', levels=100, nlabels=5):
     '''Produces a contour plot of the segregation energy at sites around a 
     dislocation. Use <levels> to control the number of contours.
@@ -210,6 +210,9 @@ def plot_energies_contour(sites, e_seg, figname, r, cmtype='viridis', refine=Fal
     y = sites[:, 2]
     triang = tri.Triangulation(x, y)
     
+    # find absolute maximum of the segregation energies
+    vmax = abs(np.array(e_seg)).max()
+    
     fig = plt.figure()
     plt.gca().set_aspect('equal')
     
@@ -218,10 +221,12 @@ def plot_energies_contour(sites, e_seg, figname, r, cmtype='viridis', refine=Fal
         # refine data for improved high-res plot
         refiner = tri.UniformTriRefiner(triang)
         tri_refi, es_refi = refiner.refine_field(e_seg, subdiv=3)
-        plt.tricontourf(tri_refi, es_refi, levels, cmap=plt.get_cmap(cmtype))
+        plt.tricontourf(tri_refi, es_refi, levels, cmap=plt.get_cmap(cmtype),
+                                            vmin=-vmax, vmax=vmax)
     else:
         # use raw data to produce contours
-        plt.tricontourf(triang, e_seg, levels, cmap=plt.get_cmap(cmtype))
+        plt.tricontourf(triang, e_seg, levels, cmap=plt.get_cmap(cmtype), 
+                                            vmin=-vmax, vmax=vmax)
         
     plt.xlabel('x ($\AA$)', size='x-large', family='serif')
     plt.ylabel('y ($\AA$)', size='x-large', family='serif')
@@ -244,7 +249,7 @@ def plot_energies_contour(sites, e_seg, figname, r, cmtype='viridis', refine=Fal
     
     return
     
-def plot_energies_scatter(sites, e_seg, figname, r, cmtype='viridis', units='eV',
+def plot_energies_scatter(sites, e_seg, figname, r, cmtype='bwr', units='eV',
                                                              figformat='tif'):
     '''Produces a scatterplot showing all of the sites for which segregation 
     energies were calculated, with each point coloured according to the 
@@ -253,6 +258,9 @@ def plot_energies_scatter(sites, e_seg, figname, r, cmtype='viridis', units='eV'
 
     x = sites[:, 1]
     y = sites[:, 2]
+    
+    # find absolute maximum of the segregation energies
+    vmax = abs(np.array(e_seg)).max()
 
     fig = plt.figure()
     plt.gca().set_aspect('equal')
